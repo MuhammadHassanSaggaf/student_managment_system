45a5dcf67dc376f7b5c84f77504c6e7c727f6fa1
Muhammad Hassan Saggaf <muhammadsaggaf2004@gmail.com>
2025-11-05 05:39:15 +0300

    Completed installation of debug_toolbar

--- SMS/settings.py
+++ SMS/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,6 +65,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 WSGI_APPLICATION = 'SMS.wsgi.application'
