3f9bf3b4585865b9dfcaa0156c284798af868674
Muhammad Hassan Saggaf <muhammadsaggaf2004@gmail.com>
2025-11-05 09:50:23 +0300

    Added content to add.html and renamed it from student_add to add.html and added css for it and links for urls

--- student_managment_system/urls.py
+++ student_managment_system/urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-  path('list/', views.list),
+  path('list/', views.list, name='list'),
+  path('add/', views.add, name='add'),
 ]
--- student_managment_system/views.py
+++ student_managment_system/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render,redirect
 # Create your views here.
 def list(request):
     return render(request, 'list.html')
+  
+def add(request):
+    if request.method == 'POST':
+        return redirect('list')  # Redirect to the list view after adding a student
+    return render(request, 'add.html')  # Render the add student form template
