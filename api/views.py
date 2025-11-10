34bce99aaf6a34ad89fa20fc99aa5aa3509f194b
Muhammad Hassan Saggaf <muhammadsaggaf2004@gmail.com>
2025-11-10 06:56:23 +0300

    Inital trial and setup of api solving readonly permission issues with drf

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(['GET'])
+def get_list(request):
+    data = {
+        "message": "Welcome to the API!",
+        "items": [1, 2, 3, 4, 5]
+    }
+    return Response(data)
